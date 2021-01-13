5f8f7a615dbc6793238dc34379fc098c15900bab
Rick Yorgason <rick@firefang.com>
2021-01-13 00:30:11 -0700

    Removed t-shirts, centred 404

--- components/MainNav.vue
+++ components/MainNav.vue
@@ -6,7 +6,6 @@
                 <a href='mailto:rick@firefang.com'><img src='~/assets/images/icons/mail.svg' :height='iconheight' alt='email'/></a>
                 <a href='https://facebook.com/skrapion'><img src='~/assets/images/icons/facebook.svg' :height='iconheight' alt='Facebook'/></a>
                 <a href='https://www.youtube.com/channel/UCLCEtL5d5botNpR4TCU_f9w'><img src='~/assets/images/icons/youtube.svg' :height='iconheight' alt='Youtube'/></a>
-                <a href='#'><img src='~/assets/images/icons/tshirt.svg' :height='iconheight' alt='Shirts!'/></a>
                 <a href='#'><img src='~/assets/images/icons/bell.svg' :height='iconheight' alt='notifications'/></a>
             </div>
         </div>
--- layouts/error.vue
+++ layouts/error.vue
@@ -28,7 +28,7 @@ export default {
     flex-direction: column;
     justify-content: center;
     align-items: center;
-    height: 100%;
+    height: 100vh;
 }
 
 h1 {
