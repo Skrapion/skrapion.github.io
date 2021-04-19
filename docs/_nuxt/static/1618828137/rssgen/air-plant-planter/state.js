6c5d383bd00dbb112ee1fe31d808250a92803ec4
Rick Yorgason <rick@firefang.com>
2021-04-19 03:30:39 -0700

    Finally got a build working

---
+++ docs/_nuxt/static/1618828137/rssgen/air-plant-planter/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1618828137",layout:"empty",error:a,serverRendered:true,routePath:"\u002Frssgen\u002Fair-plant-planter",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"4bf8cbbc"}}}}(null));
