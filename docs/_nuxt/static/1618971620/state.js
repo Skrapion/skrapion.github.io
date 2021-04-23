2427d375b48f35d369eef318f5505aaa363b5b12
Rick Yorgason <rick@firefang.com>
2021-04-22 21:02:31 -0700

    Generate

--- docs/_nuxt/static/1618971620/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1618971620",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"192921c4"}}}}(null,"\u002F"));
