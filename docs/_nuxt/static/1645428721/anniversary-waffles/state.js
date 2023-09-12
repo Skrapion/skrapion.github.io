167128757f373f7d592f85787a11f53cb91f136c
Rick Yorgason <rick@firefang.com>
2023-09-11 20:51:57 -0700

    Generated

--- docs/_nuxt/static/1645428721/anniversary-waffles/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1645428721",layout:"default",error:a,serverRendered:true,routePath:"\u002Fanniversary-waffles",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"d7322f40"}}}}(null));
